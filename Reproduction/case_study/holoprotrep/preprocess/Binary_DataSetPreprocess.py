fbe55f16aa34141b4f9139b833e983cca453e1bb
sinemzdemir <72942095+sinemzdemir@users.noreply.github.com>
2023-04-26 14:01:56 +0300

    Add files via upload

---
+++ Reproduction/case_study/holoprotrep/preprocess/Binary_DataSetPreprocess.py
@@ -0,0 +1,64 @@
+import os
+import pickle
+import datetime
+import tqdm
+import pandas as pd
+from pathlib import Path
+
+path = os.path.dirname(os.getcwd())
+def integrate_go_lables_and_representations_for_binary(
+    label_dataframe, representation_dataframe, dataset_names
+):
+    """
+        This function takes two dataframes and a dataframe name as input. First dataframe has two coloumns 'Label' and  'Entry'.
+    'Label' column includes GO Terms and 'Entry' column includes UniProt IDs. Second dataframe has a column named as
+    'Entry' in the first column, but number of the the following columns are varying based on representation vector length.
+    The function integrates these dateframes based on 'Entry' column
+
+        Parameters
+        ----------
+        label_dataframe: Pandas Dataframe
+                Includes GO Terms and UniProt IDs of annotated proteins.
+        representation_dataframe: Pandas Dataframe
+                UniProt IDs of annotated proteins and representation vectors in multicolumn format.
+        dataset_names: String
+                The name of the output dataframe which is used for saving the dataframe to the results directory.
+
+        Returns
+        -------
+        integrated_dataframe : Pandas Dataframe
+                Integrated label dataframe. The dataframe has multiple number of  columns 'Label','Entry' and 'Vector'.
+                'Label' column includes GO Terms and 'Entry' column includes UniProt ID and the following columns includes features of the protein represention vector."""
+    
+    integrated_dataframe = pd.DataFrame(columns=["Entry", "Vector"])    
+    integrated_dataframe_list = []
+    representation_cols = representation_dataframe.iloc[:, 1 : (len(representation_dataframe.columns))]
+    representation_dataset = pd.DataFrame(columns=["Entry", "Vector"])
+    for index, row in tqdm.tqdm(representation_cols.iterrows(), total=len(representation_cols)):
+        list_of_floats = [float(item) for item in list(row)]
+        representation_dataset.loc[index] = [representation_dataframe.iloc[index]["Entry"]] + [
+            list_of_floats
+        ]
+   
+    integrated_dataframe = label_dataframe.merge(
+            representation_dataset, how="inner", on="Entry"
+        )
+    integrated_dataframe.drop(
+            integrated_dataframe.filter(regex="Unname"), axis=1, inplace=True
+        )
+    
+    
+    with open(
+            str(Path(path).parent)
+            + "/"
+            + "results"
+            + "/"
+            + dataset_names
+            + "_"
+            + "binary_data"
+            + ".pickle",
+            "wb",
+        ) as handle:
+            pickle.dump(integrated_dataframe, handle)
+            
+    return integrated_dataframe
--- Reproduction/case_study/readme.md
+++ Reproduction/case_study/readme.md
@@ -1,1 +1,24 @@
+# HoloprotRep: Protein Representation Classification
+
+- HoloprotRep aims to construct models for protein function prediction Holoprotrep can concate protein representations in to prepare datasets  for training and testing models
+
+
+# How to run HoloprotRep 
+
+Step by step operation:
+  1. Clone repostory
+  2. Install dependencies(given below)
+  3. Download datasets,unzip and place the folder
+  4. Edit the configuration file (according to classification methods, for binary classification: **holoprotRep_binary_label_config.yaml** for multi label classification: **holoprotRep_multi_label_config.yaml**) by changing parameters as desired and setting paths of your file(s)
+  5. cd into holoprotrep run **HoloProtRep_binary_label.py** or **HoloProtRep_multi_label.py**
+- i.e., python **HoloProtRep_binary_label.py**
+
+# Dependencies
+ 1.	Python 3.7.3
+ 2.	pandas 1.1.4
+ 3.	scikit-learn 0.22.1.
+ 4.	Scikit-MultiLearn
+
+- Example of binary classification configuration file see documentation [binary_classification.md](binary_classification.md), for example of multilabel classification see documentation [multilabel_classification.md](multilabel_classification.md)
+
 
