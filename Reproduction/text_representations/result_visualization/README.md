31ff69bb99a506f4d3305038218ed6adb0053c79
albayrakm <70581981+albayrakm@users.noreply.github.com>
2023-06-09 15:37:58 +0300

    Add files via upload

---
+++ Reproduction/text_representations/result_visualization/README.md
@@ -0,0 +1,95 @@
+# textrep
+
+This repository contains Python scripts for analyzing prediction results and creating figures and significance tables for ontology-based function prediction.
+
+## Definition of Scripts
+
+### create_figures.py
+
+#### Functions
+
+- `create_index_from_model_name(index_names)`: Creates an index list from model names.
+- `create_pred_table(measure)`: Reads prediction results, orders them alphabetically, and creates a prediction table.
+- `get_go_pred_table_for_aspect(aspect, go_pred_table)`: Slices the prediction table by aspect and orders subgroups.
+- `prepare_figure_data_for_aspect(aspect)`: Calculates mean measures for different aspects and returns F1 weighted scores.
+- `set_colors_and_marks_for_representation_groups(ax)`: Sets colors and marks for representation groups in a plot.
+- `create_figures()`: Creates dataframes for figures and generates the figures.
+
+### create_significance.py
+
+#### Functions
+
+- `calculate_q_vals(go_pred_score_table)`: Calculates q-values using the Benjamini/Hochberg method.
+- `check_for_normality(go_pred_signinificance_score_df)`: Checks if data is drawn from a normal distribution.
+- `nan_to_zero(x)`: Replaces NaN values with zero.
+- `create_significance_tables()`: Creates significance tables for different aspects.
+
+### visualize_results.py
+
+#### Functions
+
+- `main()`: The main function that parses command-line arguments and runs the analysis.
+
+## Data
+
+The function prediction data should be provided in CSV format with the following columns:
+
+- Model: The name of the prediction model.
+- Measure: The evaluation measure used (e.g., F1-Weighted, Accuracy, Precision).
+- Aspect: The functional aspect (BP, CC, or MF).
+- Value: The value of the evaluation measure for each model and aspect.
+
+Please make sure the data is formatted correctly before running the scripts.
+
+### Options
+
+The script supports the following command-line options:
+
+- `-f` or `--figures`: Create figures from the results.
+- `-s` or `--significance`: Create significance tables from the results.
+- `-rfp` or `--resultfilespath`: Path for the result files (required).
+- `-a` or `--all`: Create both figures and significance tables.
+
+At least one option should be selected. If no options are provided, an error message will be displayed.
+
+### How to Run
+
+1. To create figures from the result files:
+
+```
+python visualize_results -f -rfp /path/to/result/files
+```
+
+2. To create significance tables from the result files:
+
+```
+python visualize_results -s -rfp /path/to/result/files
+```
+
+3. To create both figures and significance tables:
+
+```
+python visualize_results -a -rfp /path/to/result/files
+```
+
+Make sure to replace `/path/to/result/files` with the actual path to your result files.
+
+## Definition of Output
+
+The script will load the results and perform the selected actions based on the provided options. The output will be generated in the following manner:
+
+- If the `-f` or `--figures` option is selected, figures will be created and saved.
+- If the `-s` or `--significance` option is selected, significance tables will be created and saved.
+- If the `-a` or `--all` option is selected, both figures and significance tables will be created and saved.
+- The `figures` directory will contain the generated figures in PNG format.
+- The `significance` directory will contain the calculated significance scores in CSV format.
+
+## License
+
+Copyright (C)
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
---
+++ Reproduction/text_representations/result_visualization/create_figures.py
@@ -0,0 +1,187 @@
+import pandas as pd
+import glob
+import os
+import seaborn as sns
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+#import pixiedust
+pd.set_option('display.max_colwidth', -1)
+pd.options.display.max_rows = 4000
+
+result_path = ''
+
+#Classic Representations: Yellow
+#Learned Small Scale Models: Blue
+#Learned Large Scale Models: Red
+group_color_dict = {'TFIDF-UNIPROT-256':'orange','TFIDF-UNIPROT-512':'orange','TFIDF-UNIPROT-1024':'orange','TFIDF-UNIPROT-PUBMED-256':'green','TFIDF-UNIPROT-PUBMED-512':'red', 'TFIDF-UNIPROT-PUBMED-1024':'red',\
+                    'UNIPROT-BERT-AVG':'red', 'UNIPROT-BERT-POOL':'red','UNIPROT-BIOSENTVEC':'green', 'UNIPROT-BIOWORDVEC':'red', 'UNIPROT-PUBMED-BERT-AVG':'red', 'UNIPROT-PUBMED-BERT-POOL':'green',\
+                    'UNIPROT-PUBMED-BIOSENTVEC':'green','UNIPROT-PUBMED-BIOWORDVEC':'red'}
+
+def create_index_from_model_name(index_names):
+    index_list = []
+    for index_name in index_names:
+        new_name = index_name.split("_")[1:len(index_names)]
+        new_name = '_'.join(new_name)
+        index_list.append(new_name)
+    return index_list
+
+#Read prediction results and order them alphabetically. Hence they are ordered by aspect.
+def create_pred_table(measure):
+       
+    go_pred_table = pd.DataFrame()
+    for filename in sorted(glob.glob(os.path.join(result_path, '*_5cv_mean_*'))):
+            col_name = filename.split("Ontology_based_function_prediction_5cv_mean_")[-1].split(".")[0]
+            #print(col_name)
+            tmp_column = pd.read_csv(filename,sep="\t")
+            tmp_column.sort_values(tmp_column.columns[0])
+
+            go_pred_table[col_name] = tmp_column[measure]
+            index = create_index_from_model_name(list(tmp_column.iloc[:, 0]))
+    #print(go_pred_table)
+    go_pred_table["index_col"] = index
+    go_pred_table.set_index('index_col', inplace=True)
+    go_pred_table.sort_index(inplace=True)
+    #print(go_pred_table)
+    return go_pred_table
+
+# Slice dataframe by aspect and order subgroups
+def get_go_pred_table_for_aspect(aspect,go_pred_table):
+    
+    if aspect == "BP":
+        go_pred_tableBP = go_pred_table[0:9]
+        
+        new_index =  ["BP_High_Shallow", "BP_High_Normal", "BP_High_Specific",\
+                      "BP_Middle_Shallow","BP_Middle_Normal","BP_Middle_Specific",\
+                      "BP_Low_Shallow","BP_Low_Normal","BP_Low_Specific"]
+        go_pred_tableBP = go_pred_tableBP.reindex(new_index)
+        #print(go_pred_table)
+        return go_pred_tableBP
+    if aspect == "CC":
+        go_pred_tableCC = go_pred_table[9:17]
+        new_index =  ["CC_High_Shallow", "CC_High_Normal",\
+                      "CC_Middle_Shallow","CC_Middle_Normal","CC_Middle_Specific",\
+                      "CC_Low_Shallow","CC_Low_Normal","CC_Low_Specific"]
+        go_pred_tableCC = go_pred_tableCC.reindex(new_index) 
+       
+        return go_pred_tableCC
+    if aspect == "MF":
+        go_pred_tableMF = go_pred_table[17:25]
+        new_index =  ["MF_High_Shallow", "MF_High_Normal",\
+                      "MF_Middle_Shallow","MF_Middle_Normal","MF_Middle_Specific",\
+                      "MF_Low_Shallow","MF_Low_Normal","MF_Low_Specific"]
+        go_pred_tableMF = go_pred_tableMF.reindex(new_index) 
+       
+        return go_pred_tableMF
+
+
+#Calculate mean measures for different aspects also return F1 weigted scores
+def prepare_figure_data_for_aspect(aspect):
+    go_pred_tableF1 = create_pred_table("F1_Weighted")
+    go_pred_tableACC = create_pred_table("Accuracy")
+    go_pred_tablePR = create_pred_table("Precision_Weighted")
+    go_pred_tableREC = create_pred_table("Recall_Weighted")
+    go_pred_tableHAMM = create_pred_table("Hamming_Distance")
+    
+    go_pred_tableF1_aspect = get_go_pred_table_for_aspect(aspect,go_pred_tableF1)
+    go_pred_tableACC_aspect = get_go_pred_table_for_aspect(aspect,go_pred_tableACC)
+    go_pred_tablePR_aspect = get_go_pred_table_for_aspect(aspect,go_pred_tablePR)
+    go_pred_tableREC_aspect = get_go_pred_table_for_aspect(aspect,go_pred_tableREC)
+    go_pred_tableHAMM_aspect = get_go_pred_table_for_aspect(aspect,go_pred_tableHAMM)
+    #print(go_pred_tableF1_aspect)
+    go_pred_tableF1_aspect_mean = go_pred_tableF1_aspect.mean(axis = 0)
+    go_pred_tableACC_aspect_mean = go_pred_tableACC_aspect.mean(axis = 0) 
+    go_pred_tablePR_aspect_mean = go_pred_tablePR_aspect.mean(axis = 0) 
+    go_pred_tableREC_aspect_mean = go_pred_tableREC_aspect.mean(axis = 0)
+    go_pred_tableHAMM_aspect_mean = go_pred_tableHAMM_aspect.mean(axis = 0)
+    
+    #print(go_pred_tableF1_aspect_mean)
+
+
+    new_index =  ["Accuracy","F1-Weighted","Precision","Recall", "Hamming"]
+    pred_mean_df = pd.DataFrame([go_pred_tableACC_aspect_mean])
+    
+    pred_mean_df = pred_mean_df.append(go_pred_tableF1_aspect_mean, ignore_index=True)
+    pred_mean_df = pred_mean_df.append(go_pred_tablePR_aspect_mean, ignore_index=True)
+    pred_mean_df = pred_mean_df.append(go_pred_tableREC_aspect_mean, ignore_index=True)
+    pred_mean_df = pred_mean_df.append(go_pred_tableHAMM_aspect_mean, ignore_index=True)
+   
+    pred_mean_df = pred_mean_df.set_index(pd.Series(new_index))
+    pred_mean_df_table = pred_mean_df.transpose()
+    #print(pred_mean_df_table)
+    pred_mean_df_table.to_csv("tables/" + aspect + "_pred_mean_table.csv")
+
+    display_labels = ['INTERPRO2GO','UNIRULE2GO','ENSEMBL-ORTHOLOGY','BLAST','HMMER','K-SEP','APAAC','PFAM','AAC','PROTVEC',\
+    'GENE2VEC','LEARNED-VEC','MUT2VEC','TCGA-EMBEDDING','SEQVEC','CPC-PROT','BERT-BFD',\
+    'BERT-PFAM','ESMB1','ALBERT','XLNET','UNIREP','T5']
+    
+    columnsTitles = ['INTERPRO2GO','UNIRULE2GO','ENSEMBL-ORTHOLOGY','BLAST','HMMER','K-SEP','APAAC','PFAM','AAC','PROTVEC',\
+    'GENE2VEC','LEARNED-VEC','MUT2VEC','TCGA-EMBEDDING','SEQVEC','CPC-PROT','BERT-BFD',\
+    'BERT-PFAM','ESMB1','ALBERT','XLNET','UNIREP','T5']
+    
+    #pred_mean_df = pred_mean_df.reindex(columns=columnsTitles)
+    #go_pred_tableF1_aspect = go_pred_tableF1_aspect.reindex(columns=columnsTitles)
+    #go_pred_tablePR_aspect = go_pred_tablePR_aspect.reindex(columns=columnsTitles)
+    
+    #pred_mean_df.columns = display_labels
+    #go_pred_tableF1_aspect.columns = display_labels
+    #print(go_pred_tablePR_aspect)
+    return pred_mean_df,go_pred_tableF1_aspect,go_pred_tablePR_aspect
+	
+	
+def set_colors_and_marks_for_representation_groups(ax):
+    for label in ax.get_xticklabels():
+        label.set_color(group_color_dict[label.get_text()])
+        lt = label.get_text()
+        if  lt == 'MUT2VEC' or lt =='PFAM' or lt == 'GENE2VEC' or lt == 'BERT-PFAM':
+            signed_text = "^" + label.get_text()
+            label.set_text(signed_text)
+    fontproperties = {'weight' : 'bold'}
+    ax.set_xticklabels(ax.get_xticklabels(), fontproperties)
+
+def create_figures():
+
+    #Create dataframes for figures
+    pred_mean_df_BP, go_pred_tableF1_BP,go_pred_tablePR_Precision_BP = prepare_figure_data_for_aspect("BP")
+    pred_mean_df_CC, go_pred_tableF1_CC,go_pred_tablePR_Precision_CC = prepare_figure_data_for_aspect("CC")
+    pred_mean_df_MF, go_pred_tableF1_MF, go_pred_tablePR_Precision_MF = prepare_figure_data_for_aspect("MF")
+    #print(go_pred_tableF1_MF)
+    pred_mean_df_MF.T.to_csv("figures/pred_mean_df_MF.csv")
+    pred_mean_df_BP.T.to_csv("figures/pred_mean_df_BP.csv")
+    pred_mean_df_CC.T.to_csv("figures/pred_mean_df_CC.csv")
+
+
+    tables = {}
+    tables["MF"] = go_pred_tableF1_MF
+    tables["BP"] = go_pred_tableF1_BP
+    tables["CC"] = go_pred_tableF1_CC
+	
+    pred_mean_df_MF.loc['F1-Weighted'].sort_values()
+    pred_mean_df_BP.loc['F1-Weighted'].sort_values()
+    pred_mean_df_CC.loc['F1-Weighted'].sort_values()
+
+
+    g = sns.clustermap(go_pred_tableF1_MF, annot=True, cmap="YlGnBu", row_cluster=False,figsize=(15, 15))
+    ax = g.ax_heatmap
+    ax.set_xlabel("")
+    ax.set_ylabel("")
+    set_colors_and_marks_for_representation_groups(ax)
+    g.savefig('figures/func_pred_MF.png')
+
+
+    g = sns.clustermap(go_pred_tableF1_BP, annot=True, cmap="YlGnBu", row_cluster=False,figsize=(15, 15))
+    ax = g.ax_heatmap
+    ax.set_xlabel("")
+    ax.set_ylabel("")
+    set_colors_and_marks_for_representation_groups(ax)
+    g.savefig('figures/func_pred_BP.png')
+
+    g = sns.clustermap(go_pred_tableF1_CC, annot=True, cmap="YlGnBu", row_cluster=False,figsize=(15, 15))
+    ax = g.ax_heatmap
+    ax.set_xlabel("")
+    ax.set_ylabel("")
+    set_colors_and_marks_for_representation_groups(ax)
+    g.savefig('figures/func_pred_CC.png')
+
+
