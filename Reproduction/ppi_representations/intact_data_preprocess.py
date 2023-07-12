138798c4dc38a9bfbdb027bc03f729f9e3e0e6a2
isikcakmak <59953184+isikcakmak@users.noreply.github.com>
2023-07-12 17:00:13 +0300

    Update intact_data_preprocess.py

--- Reproduction/ppi_representations/intact_data_preprocess.py
+++ Reproduction/ppi_representations/intact_data_preprocess.py
@@ -51,37 +51,7 @@ intact_data_frame_filter_sorted=intact_data_frame_filter_sorted.reset_index()
 intact_data_frame_filter_sorted=intact_data_frame_filter_sorted.filter(['#ID(s) interactor A','ID(s) interactor B'])
 intact_data_frame_filter_sorted.to_excel("intact_data_frame_filter_sorted.xlsx")
 intact_data_read=pd.read_excel("intact_data_frame_filter_sorted.xlsx")
-intact_data_read_filt=intact_data_read.filter(['#ID(s) interactor A','ID(s) interactor B'])
-#intactdata_dataframe=pd.read_excel("/content/intact_data_frame_filter_sorted.xlsx")
-intactdata_dataframe=intact_data_read_filt
-#
-converted to edgelist format required for vector operation.
-protein=[]
-protein=intactdata_dataframe['#ID(s) interactor A']
-interactionprotein=intactdata_dataframe['ID(s) interactor B']
 
-list =[]
-for col in intactdata_dataframe.columns:
-    val = intactdata_dataframe[col]
-    for v in val:
-        list.append(v)
-
-lengt=intactdata_dataframe.shape[0]
-from collections import defaultdict
-temp = defaultdict(lambda: len(temp))
-res = [temp[ele] for ele in list]
-P=res[0:lengt]
-i=res[lengt:len(res)]
-intactdata_dataframe= pd.DataFrame()
-intactdata_dataframe['#ID(s) interactor A']=P
-intactdata_dataframe['ID(s) interactor B']=i
-print(intactdata_dataframe)
-intactdata_dataframe.to_csv("intactdata_dataframe_filter_human_proteins.edgelist",header=False, sep=' ', index=False)
-
-#The order of the proteins corresponding to the numbers in the edgelist was recorded.
-newlist =[]
-for i in temp.keys():
-    newlist.append(i)
 
 protein_id_df=pd.DataFrame(newlist)
 protein_id_df.to_csv("proteins_id.csv")
