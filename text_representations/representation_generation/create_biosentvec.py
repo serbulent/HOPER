3e49f2a18a8906217b43195a24d2db836cb81a87
albayrakm <70581981+albayrakm@users.noreply.github.com>
2023-09-14 09:09:12 +0300

    Update create_biosentvec.py

--- text_representations/representation_generation/create_biosentvec.py
+++ text_representations/representation_generation/create_biosentvec.py
@@ -10,12 +10,23 @@ ufiles_path = ''
 pfiles_path = ''
 stop_words = set(stopwords.words('english'))
 
+'''
+convert_dataframe_to_multi_col(representation_dataframe): This function takes a representation DataFrame as input (representation_dataframe), which has two columns: 'Entry' and 'Vector'. 
+It splits the 'Vector' column into separate columns and merges them with the 'Entry' column to create a new DataFrame with multiple columns for each dimension of the vector. 
+The resulting DataFrame with multiple columns is returned.
+'''
+
 def convert_dataframe_to_multi_col(representation_dataframe):
     entry = pd.DataFrame(representation_dataframe['Entry'])
     vector = pd.DataFrame(list(representation_dataframe['Vector']))
     multi_col_representation_vector = pd.merge(left=entry,right=vector,left_index=True, right_index=True)
     return multi_col_representation_vector
-    
+
+'''
+preprocess_sentence(text): This function takes a text string (text) as input and performs preprocessing on it. It replaces specific characters, converts the text to lowercase, 
+tokenizes the text using NLTK's word_tokenize function, removes stopwords and punctuation, and returns the preprocessed text as a string.
+'''
+
 def preprocess_sentence(text):
     text = text.replace('/', ' / ')
     text = text.replace('.-', ' .- ')
@@ -24,6 +35,13 @@ def preprocess_sentence(text):
     text = text.lower()
     tokens = [token for token in word_tokenize(text) if token not in punctuation and token not in stop_words]
     return ' '.join(tokens)
+
+'''
+create_reps(tp): This function takes a parameter tp which represents the type of data ("uniprot", "pubmed", or "uniprotpubmed"). It loads a pre-trained sentence embedding model using the sent2vec library. 
+It then iterates over files in the specified paths (ufiles_path and pfiles_path), reads the content of each file, preprocesses the content into a sentence, and obtains the sentence embedding using the loaded model. 
+The entry name and sentence embedding are stored in a DataFrame (df). The DataFrame is then converted to a multi-column representation using the convert_dataframe_to_multi_col function. 
+Finally, the resulting DataFrame is saved as a CSV file.
+'''
 
 def create_reps(tp):
     files = os.listdir(pfiles_path)
