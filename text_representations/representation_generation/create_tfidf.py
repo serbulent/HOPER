26fcdccd4cc7084b327ba4bb1f07f2cc7b4685ce
albayrakm <70581981+albayrakm@users.noreply.github.com>
2023-09-19 14:17:56 +0300

    Update create_tfidf.py

--- text_representations/representation_generation/create_tfidf.py
+++ text_representations/representation_generation/create_tfidf.py
@@ -59,9 +59,10 @@ def create_reps(tp):
         df = pd.DataFrame(denselist, columns=feature_names)
         df['Entry'] = files[i][:-4]
         df1 = df1.append(df, ignore_index=True)
-        
+
+    path=os.getcwd()
     entry = df1.Entry
-    df1.to_csv('text_representations/representation_generation/tfidf_representations/' + tp + '_tfidf_vectors.csv', index = False)
+    df1.to_csv(os.path.join(path,'text_representations/representation_generation/tfidf_representations/' + tp + '_tfidf_vectors.csv', index = False))
     
     '''
     df1.drop('Entry', inplace=True, axis=1)
@@ -71,25 +72,25 @@ def create_reps(tp):
     principalComponents = pca.fit_transform(df1)
     pca_vectors = pd.DataFrame(principalComponents)
     pca_vectors.insert(0, "Entry", entry, True)
-    pca_vectors.to_csv('text_representations/representation_generation/tfidf_representations/' + tp + '_tfidf_vectors_pca256_multi_col.csv', index = False)
+    pca_vectors.to_csv(os.path.join(path,'text_representations/representation_generation/tfidf_representations/' + tp + '_tfidf_vectors_pca256_multi_col.csv', index = False))
 
     pca = PCA(n_components=512)
     principalComponents = pca.fit_transform(df1)
     pca_vectors = pd.DataFrame(principalComponents)
     pca_vectors.insert(0, "Entry", entry, True)
-    pca_vectors.to_csv('text_representations/representation_generation/tfidf_representations/' + tp + '_tfidf_vectors_pca512_multi_col.csv', index = False)
+    pca_vectors.to_csv(os.path.join(path,'text_representations/representation_generation/tfidf_representations/' + tp + '_tfidf_vectors_pca512_multi_col.csv', index = False))
 
     pca = PCA(n_components=1024)
     principalComponents = pca.fit_transform(df1)
     pca_vectors = pd.DataFrame(principalComponents)
     pca_vectors.insert(0, "Entry", entry, True)
-    pca_vectors.to_csv('text_representations/representation_generation/tfidf_representations/' + tp + '_tfidf_vectors_pca1024_multi_col.csv', index = False)
+    pca_vectors.to_csv(os.path.join(path,'text_representations/representation_generation/tfidf_representations/' + tp + '_tfidf_vectors_pca1024_multi_col.csv', index = False))
 
     pca = PCA(n_components=2048)
     principalComponents = pca.fit_transform(df1)
     pca_vectors = pd.DataFrame(principalComponents)
     pca_vectors.insert(0, "Entry", entry, True)
-    pca_vectors.to_csv('text_representations/representation_generation/tfidf_representations/' + tp + '_tfidf_vectors_pca2048_multi_col.csv', index = False)
+    pca_vectors.to_csv(os.path.join(path,'text_representations/representation_generation/tfidf_representations/' + tp + '_tfidf_vectors_pca2048_multi_col.csv', index = False))
     '''
 def main():
     create_reps("uniprot")
