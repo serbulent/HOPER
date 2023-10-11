278c150095e4deca11d27070dab66c1f33373d88
sinemzdemir <sinemozdemir6134@gmail.com>
2023-10-11 11:36:28 +0300

    simple_ae_update

--- create_env.py
+++ create_env.py
@@ -1,6 +1,7 @@
 import os
 
 os.system("conda env create -f case_study/hoper_case_study_env.yml")
-os.system("conda env create -f /ppi_representations/hoper_PPI.yml")
-os.system("conda env create -f /text_representations/preprocess/hoper_preprocess.yml")
+os.system("conda env create -f ppi_representations/hoper_PPI.yml")
+os.system("conda env create -f text_representations/preprocess/hoper_preprocess.yml")
 os.system("conda env create -f text_representations/text_representations.yml")
+os.system("conda env create -f multimodal_representations/simple_ae_env.yml")
