30e0ac19ae76be39885ecd588e12a67a5b6ba262
albayrakm <70581981+albayrakm@users.noreply.github.com>
2025-01-30 09:45:01 +0300

    Add files via upload

--- text_representations/representation_generation/create_biobert.py
+++ text_representations/representation_generation/create_biobert.py
@@ -31,30 +31,22 @@ def create_reps(tp):
 
     data = []
     print('Generating ' + tp + ' embeddings...')
-    #for i in tqdm(range(2)):
     for i in tqdm(range(len(files))):
         file_content = ""
-        #print(files[i])
         if tp == 'uniprot':
             with open(os.path.join(ufiles_path, files[i])) as contentu:
-            #contentu = open(ufiles_path + files[i])
                 file_content = contentu.read()
         elif tp == 'pubmed':
             with open(os.path.join(pfiles_path, files[i])) as contentp:
-            #contentp = open(pfiles_path + files[i])
                 file_content = contentp.read()
         elif tp == 'uniprotpubmed':
             with open(os.path.join(ufiles_path, files[i])) as contentu, open(os.path.join(pfiles_path, files[i])) as contentp:
-            #contentu = open(ufiles_path + files[i])
-            #contentp = open(pfiles_path + files[i])
                 file_content = contentu.read() + contentp.read()
         
         inputs = tokenizer(file_content, return_tensors="pt", truncation=True, padding=True, max_length=512)
         
-        # Model ile token embeddinglerini hesapla
         with torch.no_grad():
             outputs = model(**inputs)
-            # [CLS] token embeddingini al (genellikle tüm metnin temsili olarak kullanılır)
             cls_embedding = outputs.last_hidden_state[:, 0, :] 
             flat_embedding = cls_embedding.squeeze().numpy().flatten()
             data.append([files[i][:-4], flat_embedding])
@@ -63,7 +55,6 @@ def create_reps(tp):
         gc.collect()
 
     df = pd.DataFrame(data, columns=['Entry', 'Vector']) 
-    #print(df.head())
     df = convert_dataframe_to_multi_col(df)
     df.to_csv(os.path.join(path,'text_representations/representation_generation/biobert_representations/' + tp + '_biobert_embeddings_multi_col.csv'), index = False)
    
